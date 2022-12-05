f96c58667d954625e4136027a7aa811ada3a4629
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-12-05 15:43:03 +0700

    Add project files.

---
+++ Exercise4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class node
+    {
+        public int id;
+        public node next;
+        public node(int i, node n)
+        {
+            id = i;
+            next = n;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
