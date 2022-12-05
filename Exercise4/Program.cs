eb58b197fb8781c9a21267b0c33b6b72a9117879
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-12-05 16:00:42 +0700

    add function push

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -59,6 +59,15 @@ namespace Exercise4
                 else
                     return (false);
             }
+            public void push(int element)
+            {
+                node fresh;
+                fresh = new node(element, null);
+
+                fresh.next = top;
+                top = fresh;
+                Console.WriteLine("\n" + element + "pushend");
+            }
         }
 
         static void Main(string[] args)
